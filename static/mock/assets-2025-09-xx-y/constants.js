240908fd653b130c4bb0f476d4cb8b53fd6b8d30
Duy Le <lehoangduy1911@gmail.com>
2025-09-16 09:28:12 +0700

    style: brand green; white text on green (site + mock) chore: remove legacy mock asset bundles under static/mock

--- static/mock/assets-2025-09-xx-y/constants.js
+++
@@ -1,4 +0,0 @@
-export const STORAGE_KEY = 'mockExamState@pmiacp';
-export const DEFAULT_MIN_PER_Q = { 50: 1.2, 120: 1.5 };
-export const PAGE_SIZE = 10; // 10 câu/màn
-export const LABELS = ['A', 'B', 'C', 'D'];
